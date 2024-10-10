023e2436d01b82d05a10d6ceb5a9d3be38097b8e
lucasbsaraujo91 <lucasbsaraujo91@gmail.com>
2024-10-10 17:02:41 -0300

    refatoração

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/go-chi/chi"
 
 	"temperatura_por_cep/internal/infra/api_busca_cep/api"
-	teste "temperatura_por_cep/internal/infra/api_busca_temperatura/api"
+	apiTemperatura "temperatura_por_cep/internal/infra/api_busca_temperatura/api"
 	"temperatura_por_cep/internal/infra/api_busca_temperatura/service"
 	"temperatura_por_cep/internal/usecase"
 )
@@ -17,90 +16,63 @@ import (
 func AddressHandler(fetcher api.AddressFetcher) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		cep := chi.URLParam(r, "cep")
-
-		addressUseCase := usecase.AddressUseCase{Fetcher: fetcher}
-		address, err := addressUseCase.GetAddressByZipCode(usecase.GetAddressInputDTO{ZipCode: cep})
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error fetching address: %v", err), http.StatusInternalServerError)
-			return
-		}
-
-		response, err := json.Marshal(address)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error marshaling address: %v", err), http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(response)
+		handleAddressRequest(w, cep, fetcher)
 	}
 }
 
-// ew function to fetch weather data
-func WeatherHandler(service *service.WeatherService) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		city := chi.URLParam(r, "city")
-		country := chi.URLParam(r, "country")
-
-		city = strings.ReplaceAll(city, " ", "+")
-		country = strings.ReplaceAll(country, " ", "+")
-
-		weatherData, err := service.FetchWeatherByCity(city, country) // Chama o método do serviço
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Errorrr fetching weather: %v", err), http.StatusInternalServerError)
-			return
-		}
-
-		response, err := json.Marshal(weatherData)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error marshaling weather data: %v", err), http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(response)
+func handleAddressRequest(w http.ResponseWriter, cep string, fetcher api.AddressFetcher) {
+	addressUseCase := usecase.NewAddressUseCase(fetcher)
+	address, err := addressUseCase.GetAddressByZipCode(usecase.GetAddressInputDTO{ZipCode: cep})
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error fetching address: %v", err), http.StatusInternalServerError)
+		return
 	}
+
+	sendJSONResponse(w, address)
+}
+
+func sendJSONResponse(w http.ResponseWriter, data interface{}) {
+	response, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error marshaling data: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
 }
 
 func main() {
 	r := chi.NewRouter()
 
 	fetcher := &api.DefaultAddressFetcher{}
-
-	apiKey := "4457481f588941748b8232000240910"       // Insira sua chave da API
-	weatherFetcher := teste.NewWeatherFetcher(apiKey) // Passa a chave para o fetcher
+	apiKey := "4457481f588941748b8232000240910" // Insira sua chave da API
+	weatherFetcher := apiTemperatura.NewWeatherFetcher(apiKey)
 	weatherService := service.NewWeatherService(apiKey, weatherFetcher)
 
-	// Criar a instância do WeatherUseCase
 	addressUseCase := usecase.NewAddressUseCase(fetcher)
+	// Rota para buscar o clima pelo CEP
 	weatherUseCase := &usecase.WeatherUseCase{
 		AddressUseCase: addressUseCase,
 		WeatherService: weatherService,
 	}
-
-	// Rota para buscar o clima pelo CEP
 	r.Get("/weather/zipcode/{zipCode}", func(w http.ResponseWriter, r *http.Request) {
 		zipCode := chi.URLParam(r, "zipCode")
-		weather, err := weatherUseCase.GetWeatherByZipCode(zipCode)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error fetching weather: %v", err), http.StatusInternalServerError)
-			return
-		}
-
-		response, err := json.Marshal(weather)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error marshaling weather data: %v", err), http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(response)
+		handleWeatherByZipCode(w, zipCode, weatherUseCase)
 	})
 
 	r.Get("/address/{cep}", AddressHandler(fetcher))
 
 	http.ListenAndServe(":8060", r)
+}
+
+func handleWeatherByZipCode(w http.ResponseWriter, zipCode string, weatherUseCase *usecase.WeatherUseCase) {
+	weather, err := weatherUseCase.GetWeatherByZipCode(zipCode)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error fetching weather: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	sendJSONResponse(w, weather)
 }
